04c7dba1070bb62170f2a329456ecd38ea68f725
Hayden <hdrd74@gmail.com>
2025-10-24 10:57:51 +0300

    ddb layer update

--- layers/ddb/src/nodejs/dbHelper.mjs
+++ layers/ddb/src/nodejs/dbHelper.mjs
@@ -1,5 +1,1 @@
-// Minimal DDB helper stub for PoC layer shape
-// Note: No external dependencies; not used by the function yet
-
-export const ping = () => "pong";
-
+export const ping = () => "pong 1";
